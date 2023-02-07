2a597957fb7c91c4e76b1918000b1071de0190ae
MaheshbabuReddy <maheshsvm123@gmail.com>
2023-02-07 16:31:23 +0530

    Adding all files

---
+++ Terraform/Main network.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  
+
+  tags = {
+    Name = "my vpc "
+  }
+
+}
+#this is for subnet
+ resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet 1"
+  }
+ }
+ resource "aws_s3_bucket" "example"{
+    bucket = "preebuck"
+  }
